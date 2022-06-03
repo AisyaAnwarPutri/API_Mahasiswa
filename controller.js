b48c37a150cb4e755c0418994ee08364b85621ad
AISYA ANWAR PUTRI <aisyanwarputri@gmail.com>
2022-06-03 13:01:58 +0700

    API Mahasiswa

---
+++ controller.js
@@ -0,0 +1,183 @@
+'use strict';
+
+var response = require('./res');
+var connection = require('./conn');
+// var encrypt = require('./encrypt_password');
+
+exports.index = function(req, res) {
+    response.ok("Hello, pesan ini dari sisi server NodeJS Restful API!", res)
+};
+
+exports.mahasiswa = function(req, res) {
+    connection.query('SELECT * FROM tb_mahasiswa', function (error, rows, fields){
+        var message;
+        if(error){
+            message = "Error ketika menampilkan semua data Mahasiswa!";
+            response.gagal(message, res);
+        } else{
+            message = "Sukses menampilkan semua data Mahasiswa";
+            response.ok_with_data(message, rows, res);
+        }
+    });
+};
+
+exports.findMahasiswa = function(req, res) {
+    
+    var nim = req.params.nim;
+    var message;
+    
+    if (!nim) {
+        message = "Kehilangan beberapa parameter yang dibutuhkan!";
+        return response.gagal(message, res);
+    }
+    connection.query('SELECT * FROM tb_mahasiswa where nim = ?', [ nim ], function (error, rows, fields){
+        if(error){
+            message = "Error ketika mencari nim yang dimaksud!";
+            response.gagal(message, res);
+        } else{
+            if(rows.length > 0){
+                message = "Nim ditemukan";
+                response.ok_with_data(message, rows[0], res);
+            }else{
+                message = "Nim tidak ditemukan!";
+                response.gagal(message, res);
+            }
+        }
+    });
+};
+
+// exports.loginMahasiswa = async function(req, res) {
+    
+//     var email = req.query.email;
+//     var myPassword = req.query.password;
+//     var message;
+    
+//     if (!email || !myPassword) {
+//         message = "Kehilangan beberapa parameter yang dibutuhkan!";
+//         return response.gagal(message, res);
+//     }
+    
+    
+//     await connection.query('SELECT * FROM tb_mahasiswa where email = ?', [ email ], function(error, rows, fields){
+//         if(rows.length > 0){
+//             if(encrypt.compareHashPassword(myPassword, rows[0].password)){
+//                 message = "Login berhasil";
+//                 response.ok_with_data(message, rows[0], res);
+//             }else{
+//                 message = "Password mahasiswa tidak cocok!";
+//                 response.gagal(message, res);
+//             }
+//         }else{
+//             message = "mahasiswa tidak terdaftar!";
+//             response.gagal(message, res);
+//         }
+//     });
+// };
+
+exports.registerMahasiswa = async function(req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var email = req.body.email;
+    var no_hp = req.body.no_hp;
+    var jurusan = req.body.jurusan;
+    var prodi = req.body.prodi;
+    var kelas = req.body.kelas;
+    var paralel = req.body.paralel;
+    var message;
+    
+    if (!nim || !nama || !email || !no_hp || !jurusan || !prodi || !kelas|| !paralel) {
+        message = "Kehilangan beberapa parameter yang dibutuhkan!";
+        return response.gagal(message, res);
+    }
+    
+    await connection.query('SELECT * FROM tb_mahasiswa where nim = ?', [ nim ], function(error, rows, fields){
+        if(rows.length > 0){
+            message = "Nim user telah terdaftar, silahkan pilih yang lain!";
+            response.gagal(message, res);
+        }else{
+            // var encrypt_password = encrypt.saltHashPassword(plain_password);
+            connection.query('INSERT INTO tb_mahasiswa (nim, nama, email, no_hp, jurusan, prodi, kelas, paralel) values (?,?,?,?,?,?,?,?)', [ nim, nama, email, no_hp, jurusan, prodi, kelas, paralel ], function (error, rows, fields){
+                if(error){
+                    message = "Error ketika menambahkan mahasiswa baru!";
+                    response.gagal(message, res);
+                } else{
+                    message = "Berhasil menambahkan mahasiswa baru";
+                    response.ok(message, res);
+                }
+            });
+        }
+    });
+};
+
+exports.updateMahasiswa = async function(req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var email = req.body.email;
+    var no_hp = req.body.no_hp;
+    var jurusan = req.body.jurusan;
+    var prodi = req.body.prodi;
+    var kelas = req.body.kelas;
+    var paralel = req.body.paralel;
+    var message;
+    
+    if (!nim || !nama || !email || !no_hp || !jurusan || !prodi || !kelas || !paralel) {
+        message = "Kehilangan beberapa parameter yang dibutuhkan!";
+        return response.gagal(message, res);
+    }
+    
+    var sql_query;
+    var params;
+    if(nim == ""){
+        sql_query = 'UPDATE tb_mahasiswa SET nim = ?, nama = ?, email = ?, no_hp = ?, jurusan = ?, prodi = ? WHERE id = ?';
+        params = [ nim, nama, email, no_hp, jurusan, prodi, mahasiswa_id ]
+    }else{
+        sql_query = 'UPDATE tb_mahasiswa SET nim = ?, nama = ?, email = ?, no_hp = ?, jurusan = ?, prodi = ?, kelas = ? WHERE id = ?';
+        // var encrypt_password = encrypt.saltHashPassword(plain_password);
+        params = [ nim, nama, email, no_hp, jurusan, prodi, kelas, paralel ]
+    }
+    
+    await connection.query('SELECT * FROM tb_mahasiswa where email = ? and nim <> ?', [ email, nim ], function(error, rows, fields){
+        if(rows.length > 0){
+            message = "Email mahasiswa telah terdaftar, silahkan pilih yang lain!";
+            response.gagal(message, res);
+        }else{
+            connection.query(sql_query, params, function (error, rows, fields){
+                if(error){
+                    message = "Error ketika mengubah data mahasiswa!";
+                    response.gagal(message, res);
+                } else{
+                    message = "Berhasil mengubah data mahasiswa";
+                    response.ok(message, res);
+                }
+            });
+        }
+    });
+};
+
+exports.deleterMahasiswa = async function(req, res) {
+    
+    var mahasiswa_nim = req.params.nim;
+    var message;
+    
+    if (!mahasiswa_nim) {
+        message = "Kehilangan beberapa parameter yang dibutuhkan!";
+        return response.gagal(message, res);
+    }
+    
+    await connection.query('SELECT * FROM tb_mahasiswa where nim = ?', [ mahasiswa_nim ], function(error, rows, fields){
+        if(rows.length > 0){
+            connection.query('DELETE FROM tb_mahasiswa WHERE nim = ?', [ mahasiswa_nim ], function (error, rows, fields){
+                if(error){
+                    message = "Error ketika mengahapus mahasiswa!";
+                    response.gagal(message, res);
+                } else{
+                    message = "Berhasil mengahapus mahasiswa";
+                    response.ok(message, res);
+                }
+            });
+        }else{
+            message = "mahasiswa tidak ditemukan!";
+            response.gagal(message, res);
+        }
+    });
+};
---
+++ routes.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = function(app) {
+ var todoList = require('./controller');
+ 
+ //routing untuk index
+ app.route('/')
+ .get(todoList.index);
+
+ //routing untuk user
+ app.route('/mahasiswa')
+ .get(todoList.mahasiswa);
+ 
+ app.route('/mahasiswa/:nim')
+ .get(todoList.findMahasiswa);
+ 
+//  app.route('/login')
+//  .get(todoList.loginUser);
+
+ app.route('/mahasiswa')
+ .post(todoList.registerMahasiswa);
+
+ app.route('/mahasiswa')
+ .put(todoList.updateMahasiswa);
+ 
+ app.route('/mahasiswa/:nim')
+ .delete(todoList.deleterMahasiswa);
+ 
+//  //routing untuk jasa
+//  app.route('/services')
+//  .post(todoList.addService);
+ 
+//  app.route('/services')
+//  .get(todoList.getServices);
+ 
+//  app.route('/services/:id')
+//  .get(todoList.getUserServices);
+ 
+//  app.route('/services/:id_jasa')
+//  .delete(todoList.deleteService);
+ 
+//  app.route('/services')
+//  .put(todoList.editServiceReplaceImage);
+ 
+//  app.route('/services/:id_jasa')
+//  .put(todoList.editService);
+};
